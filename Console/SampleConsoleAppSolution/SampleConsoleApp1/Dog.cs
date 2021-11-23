6e897898671ad6a376daf581d186392597177ea4
SimonasB <93397193+SimasBir@users.noreply.github.com>
2021-11-23 19:09:48 +0200

    Added dog class

---
+++ Console/SampleConsoleAppSolution/SampleConsoleApp1/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp1
+{
+    public class Dog
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Bark");
+        }
+        public static void StaticBark()
+        {
+            Console.WriteLine("Static,Bark");
+        }
+    }
+}
--- Console/SampleConsoleAppSolution/SampleConsoleApp1/Program.cs
+++ Console/SampleConsoleAppSolution/SampleConsoleApp1/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// var randomNumber = "5" + true;
+
+using SampleConsoleApp1;
+using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var dog1 = new Dog();
+            var dog2 = new Dog();
+
+            dog1.Bark();
+            dog2.Bark();
+
+            Dog.StaticBark();
+                        
+            //var randomNumber = "5";
+            var randomNumber = GetNumber();
+            PrintInfo(randomNumber);
+        }
+
+        public static void PrintInfo(int info) /*Arba string*/
+        {
+            Console.WriteLine(info);
+        }
+
+        public static int GetNumber()
+        {
+            return 5*2;
+        }
+    }
+}
+            
