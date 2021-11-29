b09b31642b664f8d2da3a5c7bafc38249f3ece11
SimonasB <93397193+SimasBir@users.noreply.github.com>
2021-11-29 18:49:16 +0200

    Update function added

--- Console/C5BookStoreApplication/C5BookStoreApplication/Program.cs
+++ Console/C5BookStoreApplication/C5BookStoreApplication/Program.cs
@@ -13,7 +13,7 @@ namespace C5BookStoreApplication
             bool exit = false;
             while (exit == false)
             {
-                Console.WriteLine("Write your commands (add, list, delete or exit): ");
+                Console.WriteLine("Write your commands (add, list, delete, update or exit): ");
                 string command = Console.ReadLine();
                 switch (command)
                 {
@@ -21,7 +21,7 @@ namespace C5BookStoreApplication
 
                         Console.WriteLine("Enter the title");
                         string title = Console.ReadLine();
-                        foreach(var book in library)
+                        foreach (var book in library)
                         {
                             if (title == book.Title)
                             {
@@ -30,23 +30,25 @@ namespace C5BookStoreApplication
                             }
 
                         }
-                        
+                        // var uniqueCounter= library.Where(x => x.Title == Title).ToList().Count;
+                        // Console.WriteLine(uniqueCounter);
+
                         Console.WriteLine("Enter the description");
                         string description = Console.ReadLine();
                         Console.WriteLine("Enter the amount");
                         try
                         {
-                        int amount = Convert.ToInt32(Console.ReadLine());
-                        var book = new Books(title, description, amount);
-                        library.Add(book);
+                            int amount = Convert.ToInt32(Console.ReadLine());
+                            var book = new Books(title, description, amount);
+                            library.Add(book);
                             break;
                         }
-                        catch 
+                        catch
                         {
                             Console.WriteLine("Not a number, try again");
                             break;
                         }
-                        End: break;
+                    End: break;
 
                     case "list":
                         foreach (var Books in library)
@@ -60,11 +62,45 @@ namespace C5BookStoreApplication
                         break;
 
                     case "delete":
-                        Console.WriteLine("Enter the title you wish to delete");
+                        Console.WriteLine("Enter the title you wish to delete:");
                         string delete = Console.ReadLine();
                         library = library.Where(t => t.Title != delete).ToList();
+                        //library = library.removeAll(x => x.Name == delete)
                         break;
 
+                    case "update":
+
+                        Console.WriteLine("Enter which title you wish to update (new amount is necessary):");
+                        try
+                        {
+                            string update = Console.ReadLine();
+                            //library = library.Where(t => t.Title == update).ToList();
+                            bool alreadyExist = library.Exists(t=>t.Title==update);
+                            if (alreadyExist)
+                            {
+                            Console.WriteLine("Enter new description:");
+                            string newDesc = Console.ReadLine();
+                            Console.WriteLine("Enter new amount:");
+                            int newAm = Convert.ToInt32(Console.ReadLine());
+                            if (newDesc != "")
+                            {
+                                library.Where(t => t.Title == update).ToList().ForEach(x => x.Description = newDesc);
+                            }
+
+                            library.Where(t => t.Title == update).ToList().ForEach(x => x.Amount = newAm);
+                            }
+                            else
+                            {
+                                Console.WriteLine("No such entry");
+                            }
+
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Update is impossible");
+                            break;
+                        }
+                        break;
                     default:
                         break;
                 }
