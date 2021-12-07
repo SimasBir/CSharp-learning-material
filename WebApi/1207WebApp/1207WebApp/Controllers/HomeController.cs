20dd7e4a3728aef317a8041f6934c8a157cfbfc2
SimonasB <93397193+SimasBir@users.noreply.github.com>
2021-12-07 19:24:10 +0200

    FirstMvcApplication

---
+++ WebApi/1207WebApp/1207WebApp/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using _1207WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1207WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult AboutMe()
+        {
+            var personModel = new PersonModel()
+            {
+                Name = "Name Namely2"
+            };
+            return View(personModel);
+        }
+        // Display a page with the form
+        public IActionResult DisplaySubmitData()
+        {
+            var emptyModel = new PersonModel()
+            {
+                Name = "fill the blanks"
+            };
+            return View(emptyModel);
+        }
+
+        // will receive filled model and will save to file
+        public IActionResult SendSubmitData(PersonModel model)
+        {
+            System.IO.File.WriteAllText("test.txt", model.Name);
+            return RedirectToAction("DisplaySubmitData");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
