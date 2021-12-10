692f96b6d91bfeee8aec1d6c69741ab7bcb0a3d5
SimonasB <93397193+SimasBir@users.noreply.github.com>
2021-12-10 18:41:57 +0200

    CRUD update, clean up

--- WebApi/1209ZooApp/1209ZooApp/Controllers/ZooController.cs
+++ WebApi/1209ZooApp/1209ZooApp/Controllers/ZooController.cs
@@ -9,28 +9,20 @@ namespace _1209ZooApp.Controllers
 {
     public class ZooController : Controller
     {
-
-
-        private readonly ILogger<ZooController> _logger;
         private readonly ZooService _zooService;
         private SqlConnection _sqlConnection;
 
-
-
-        public ZooController(ILogger<ZooController> logger, ZooService zooService, SqlConnection sqlConnection)
+        public ZooController(ZooService zooService, SqlConnection sqlConnection)
         {
-            _logger = logger;
             _zooService = zooService;
             _sqlConnection = sqlConnection;
-
         }
-        // GET: ZooController
+
         public ActionResult Index()
         {
             List<ZooModel> zooList = _zooService.GetAll(_sqlConnection);
             return View(zooList);
         }
-
 
         public ActionResult AnimalManagement()
         {
@@ -42,76 +34,17 @@ namespace _1209ZooApp.Controllers
             _zooService.Add(model, _sqlConnection);
             return RedirectToAction("AnimalManagement");
         }
+
         public ActionResult SendDelete(string name)
         {
             _zooService.Delete(name, _sqlConnection);
             return RedirectToAction("AnimalManagement");
         }
+
         public ActionResult SendUpdate(ZooModel model)
         {
             _zooService.Update(model, _sqlConnection);
             return RedirectToAction("AnimalManagement");
-        }
-
-
-
-        // GET: ZooController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-        // POST: ZooController/Create
-        [HttpPost]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ZooController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ZooController/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ZooController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ZooController/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
--- WebApi/1209ZooApp/1209ZooApp/Services/ZooService.cs
+++ WebApi/1209ZooApp/1209ZooApp/Services/ZooService.cs
@@ -8,7 +8,6 @@ namespace _1209ZooApp
     {
         public List<ZooModel> GetAll(SqlConnection sqlConnection)
         {
-
             sqlConnection.Open();
             List<ZooModel> sqlZoo = new List<ZooModel>();
             using (var command = new SqlCommand("SELECT * from dbo.Zoo", sqlConnection))
@@ -20,10 +19,12 @@ namespace _1209ZooApp
                         sqlZoo.Add(new ZooModel() { Name = dr.GetString(0), Description = dr.GetString(1), Gender = dr.GetString(2), Age = dr.GetInt32(3) });
                     }
                 }
+
                 sqlConnection.Close();
                 return sqlZoo;
             }
         }
+
         public void Add(ZooModel model, SqlConnection sqlConnection)
         {
             if (model.Name != null)
@@ -44,7 +45,12 @@ namespace _1209ZooApp
             {
                 sqlConnection.Open();
                 SqlDataAdapter adapter = new SqlDataAdapter();
-                //string sqlCommand = $"INSERT INTO dbo.Zoo (Name, Description, Gender, Age) VALUES ('{model.Name}','{model.Description}','{model.Gender}',{model.Age})";
+                //if (name != null)
+                //{
+                //    string sqlCommand = $"UPDATE dbo.Zoo SET Name = '{model.Name}', Description = '{model.Description}', Gender = '{model.Gender}', Age = {model.Age} WHERE Name = '{model.Name}'";
+                //    adapter.InsertCommand = new SqlCommand(sqlCommand, sqlConnection);
+                //}
+                string sqlCommand = $"UPDATE dbo.Zoo SET Description = '{model.Description}', Gender = '{model.Gender}', Age = {model.Age} WHERE Name = '{model.Name}'";
                 adapter.InsertCommand = new SqlCommand(sqlCommand, sqlConnection);
                 adapter.InsertCommand.ExecuteNonQuery();
 
@@ -58,7 +64,7 @@ namespace _1209ZooApp
             {
                 sqlConnection.Open();
                 SqlDataAdapter adapter = new SqlDataAdapter();
-                //string sqlCommand = $"INSERT INTO dbo.Zoo (Name, Description, Gender, Age) VALUES ('{model.Name}','{model.Description}','{model.Gender}',{model.Age})";
+                string sqlCommand = $"DELETE FROM dbo.Zoo WHERE name = '{name}'";
                 adapter.InsertCommand = new SqlCommand(sqlCommand, sqlConnection);
                 adapter.InsertCommand.ExecuteNonQuery();
 
