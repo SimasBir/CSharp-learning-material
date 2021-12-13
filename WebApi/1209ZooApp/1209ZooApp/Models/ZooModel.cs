d30cd811fc3ca1b7d10b0ec65a5fb7424bce5131
SimonasB <93397193+SimasBir@users.noreply.github.com>
2021-12-13 18:33:51 +0200

    Validation attempt start

--- WebApi/1209ZooApp/1209ZooApp/Models/ZooModel.cs
+++ WebApi/1209ZooApp/1209ZooApp/Models/ZooModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace _1209ZooApp.Models
 {
     public class ZooModel
     {
+        [Required]
+        [StringLength(10, MinimumLength = 1)]
         public string Name { get; set; }
+        [StringLength(20)]
         public string Description { get; set; }
+        [StringLength(10)]
         public string Gender { get; set; }
         public int Age { get; set; }
 
