9e8782ee7864d6d41b04f592c31bbec979b1d2f4
SimonasB <93397193+SimasBir@users.noreply.github.com>
2021-12-15 18:49:13 +0200

    EF Core project start

---
+++ EFCore/1215EFCTodoListApp/1215EFCTodoListApp/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using _1215EFCTodoListApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1215EFCTodoListApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ EFCore/1215EFCTodoListApp/1215EFCTodoListApp/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1215EFCTodoListApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
