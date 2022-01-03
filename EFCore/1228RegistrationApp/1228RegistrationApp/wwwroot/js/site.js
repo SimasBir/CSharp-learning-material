3d38431a9fac7787c5d4903fb29013503a84455e
SimonasB <93397193+SimasBir@users.noreply.github.com>
2022-01-03 18:25:21 +0200

    Added JS functionallity

--- EFCore/1228RegistrationApp/1228RegistrationApp/wwwroot/js/site.js
+++ EFCore/1228RegistrationApp/1228RegistrationApp/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Demo using plain javascript
+var button = document.getElementsByClassName('Inactive');
+var clickBtn = document.getElementsByClassName('click')[0];
+
+// Disable the button on initial page load
+button.disabled = true;
+for (var i = 0; i < button.length; i++) {
+    button[i].disabled = true;
+}
+//add event listener
+clickBtn.addEventListener('click', function (event) {
+    for (var i = 0; i < button.length; i++) {
+        button[i].disabled = !button[i].disabled;
+    }
+});
+
+//button.addEventListener('click', function (event) {
+//    alert('Enabled!');
+//});
